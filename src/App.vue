acdfcc4d2a009645cc766685d530d537b98783e3
Luiz França <luiz.ramos@alfasoft.pt>
2022-06-15 12:52:18 -0300

    created whole app

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <a class="title" v-if="isLaraBirthday">SIM</a>
+    <a class="title" v-else>NÃO</a>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "App",
+  components: {},
+  computed: {
+    currentDate() {
+      return moment().format("DD/MM");
+    },
+    isLaraBirthday() {
+      return this.currentDate == "02/07";
+    },
+  },
+};
+</script>
+
+<style>
+.title {
+  display: inline-block;
+  margin-top: 200px;
+  font-weight: bold;
+  font-size: 120pt;
+  font-family: Arial, sans-serif;
+  text-decoration: none;
+  cursor: default;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+</style>
